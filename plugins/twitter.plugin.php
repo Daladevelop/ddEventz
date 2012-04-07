d379df3057e1f3268797db828cf7cc4714343139
Henrik Carlsson <henrik.carlsson@me.com>
2012-04-07 23:10:39 +0200

    Fixed more

--- plugins/twitter.plugin.php
+++ plugins/twitter.plugin.php
@@ -16,11 +16,10 @@ class twitter extends plugin {
 	public function setParameters($parameters) {
 		// Set the parameters for the query to twitter
 		if (isset($parameters['tag'])) {
-			$this->tag = $tag;	
+			$this->tag = $parameters['tag'];
 		}
 
 		if ((isset($parameters['lat'])) && (isset($parameters['lon']))) {
-			//$this->geo['lat' => $parameters['lat'], 'lon' => $parameters['lon']];
 			$this->geo['lat'] = $parameters['lat'];
 			$this->geo['lon'] = $parameters['lon'];
 
@@ -49,7 +48,7 @@ class twitter extends plugin {
 		$base_query = 'http://search.twitter.com/search.json?q=';
 
 		if (isset($this->tag)) { // Make a search query by tag
-			$this->query = $base_query . urlencode($this->tag);
+			$this->query = $base_query . urlencode('#' . $this->tag);
 		}
 	}
 
@@ -58,7 +57,6 @@ class twitter extends plugin {
 	 */
 	private function requestData() {
 		// Set up cURL 
-		echo $this->query;
 		$curl = curl_init($this->query); 
 		curl_setopt($curl, CURLOPT_POST, false); 
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
