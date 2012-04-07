fb561c39fd4b3cb4d809fffc96fbcdc5fd9a5d2e
Emil Österlund <emil@larsemil.se>
2012-04-07 22:44:27 +0200

    config

--- config.php
+++ config.php
@@ -7,29 +7,18 @@ ini_set('log_errors', 1);
 error_reporting(E_ALL);
 
 
-
-
-
-
-
-
-
 //First time settings fo loading feed:
-global $parameters = array('tag' => "#daladevelop",'lag' => 60.6054, 'long' =>  15.6535, 'distance' => 5000);
-
-
-
-
-
-
+global $parameters = array(
+	'tag' => "daladevelop",
+	'lag' => 60.6054, 
+	'long' =>  15.6535, 
+	'distance' => 5000);
 
 //All config settings are done, lets do this!
 
 //load all requirements
 require_once('classes/ddeventz.php');
 require_once('classes/loadplugins.php');
-	
-
 
 //Load all social plugins
 pluginLoader::all();
