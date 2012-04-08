9f303f197ce9d6d1d70f8fe74a9b61b44acf494b
Emil Österlund <emil@larsemil.se>
2012-04-08 02:11:09 +0200

    Fixed rss plugin.

---
+++ plugins/rss.plugin.php
@@ -0,0 +1,59 @@
+<?php
+require_once('lib/simplepie/simplepie.inc');
+
+class rss extends plugin{
+
+    public function __construct()
+    {   
+        $this->url="http://larsemil.se/feed/";
+
+    }
+
+    public function setParameters($parameters)
+    {
+		//we get some parameters, but as RSS i dont think we need them do we? 
+
+    }
+
+    public function getFeed()
+    {
+        return $this->parseRSS();
+
+    }
+
+    private function parseRSS()
+	{
+		$feed = new SimplePie();
+
+		// Set which feed to process.
+		$feed->set_feed_url($this->url);
+
+		// Run SimplePie.
+		$feed->init();
+
+		// This makes sure that the content is sent to the browser as text/html and the UTF-8 character set (since we didn't change it).
+		$feed->handle_content_type();		
+		$items = array();
+		foreach ($feed->get_items() as $key=>$item):
+				array_push($items, array(
+					'id' => $item->get_title(),
+					'metadata' => array(
+									'service' => 'RSS',
+									'handle' => $feed->get_title(),
+									'URI' => $item->get_permalink() 	
+								),
+					'content' => $item->get_description(),
+					'time' => $item->get_date()
+				));
+		endforeach;
+		return json_encode($items); 
+
+
+	}
+
+
+}
+
+
+?>
+
