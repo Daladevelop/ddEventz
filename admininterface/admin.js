8ec791dc4f60454029371b1a820673fd8bc5abc0
Emil Österlund <emil@larsemil.se>
2012-11-08 19:34:45 +0100

    Added a prototype of how i do think of the admin interface

--- admininterface/admin.js
+++ admininterface/admin.js
@@ -1,39 +1,13 @@
-function handleDragStart(e) {
-  this.style.opacity = '0.4';  // this / e.target is the source node.
-}
-
-
-function handleDragOver(e) {
-  if (e.preventDefault) {
-    e.preventDefault(); // Necessary. Allows us to drop.
-  }
-
-  e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
-
-  return false;
-}
-
-function handleDragEnter(e) {
-  // this / e.target is the current hover target.
-  this.classList.add('over');
-}
-
-function handleDragLeave(e) {
-  this.classList.remove('over');  // this / e.target is previous target element.
-}
+$('aside#pluginlist div.plugin').draggable();
+$('section#activeplugins').droppable({
+	drop:function(event,ui){
+		$(this).addClass('over'); 
+		$(this).append($(event.srcElement).clone().removeAttr('style'));
+		$(event.srcElement).css("left",'');
+	   	$(event.srcElement).css("top",''); 	
+		
+	}
 
 
 
-
-
-var cols = document.querySelectorAll('aside#pluginlist div.plugin');
-[].forEach.call(cols, function(col) {
-  col.addEventListener('dragstart', handleDragStart, false);
-});
-
-var droptarget = document.querySelectorAll('section#activeplugins');
-[].forEach.call(droptarget, function(area) {
-	area.addEventListener('dragenter', handleDragEnter, false);
-	area.addEventListener('dragover', handleDragOver, false);
-	area.addEventListener('dragleave', handleDragLeave, false);
 });
